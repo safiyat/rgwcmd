d8abcba42f2fbfc207734f56ced1cb4a8fc6dd09
Safiyat Reza <md.reza@snapdeal.com>
2016-02-01 15:22:58 +0530

    Added skeleton.

---
+++ rgwcmd/keys.py
@@ -0,0 +1,10 @@
+class Keys(object):
+
+    def __init__(self, connection):
+          self._conn = connection
+
+    def add_key(self, uid, access_key=None, secret_key=None):
+        pass
+
+    def remove_key(self, uid, access_key):
+        pass
--- rgwcmd/user.py
+++ rgwcmd/user.py
@@ -1,75 +1,25 @@
 """User service for Ceph RADOS Gateway (radosgw)."""
 
-class User:
+class User(object):
 
-    def __init__(self, uid='', display_name='', email='', access_key='',
-                 secret_key='', user_caps='', max_buckets=1000, suspended=''):
+    def __init__(self):
         """User class constructor."""
-        self._uid = uid
-        self._display_name = display_name
-        self._email = email
-        self._access_key = access_key
-        self._secret_key = secret_key
-        self._user_caps = user_caps
-        self._max_buckets = max_buckets
-        self._suspended = suspended
-
-    def build_params(self, uid='', display_name='', email='', access_key='',
-                      secret_key='', user_caps='', max_buckets='', suspended='',
-                      purge_data=''):
-        """Function to build the parameters supplied"""
-        param_string = ''
-
-        if uid:
-            param_string += 'uid=' + uid + '&'
-
-        if display_name:
-            param_string += 'display-name=' + display_name + '&'
-
-        if email:
-            param_string += 'email=' + email + '&'
-
-
-        if access_key:
-            param_string += 'access-key=' + access_key + '&'
-
-        if secret_key:
-            param_string += 'secret-key=' + secret_key + '&'
-
-
-        if user_caps:
-            param_string += 'user-caps=' + user_caps + '&'
-
-        if max_buckets:
-            param_string += 'max-buckets=' + str(max_buckets) + '&'
-
-        if suspended:
-            param_string += 'suspended=' + suspended + '&'
-
-        if purge_data:
-            param_string += 'purge-data=' + purge_data + '&'
-
-        param_string += 'format=json'
-
-        return param_string
+        pass
 
     def check_params(self, param_string):
         pass
 
-
-    def create_user(self, uid, display_name, email='', access_key='',
-                    secret_key='', user_caps='', max_buckets=1000, suspended=''):
+    def create_user(self, uid, display_name, email=None, key_type='s3', access_key=None,
+                    secret_key=None, user_caps=None, generate_key=True, max_buckets=1000,
+                    suspended=False):
         """Create a new user. By default, an S3 key pair will be created automatically."""
-        # if access_key and secret_key:
-        method = 'PUT'
         endpoint = '/admin/user'
-        params = self.build_params(uid=uid, display_name=display_name, email=email,
-                               access_key=access_key, secret_key=secret_key,
-                               user_caps=user_caps, max_buckets=max_buckets,
-                               suspended=suspended)
-
-        return method, endpoint, params
-
+        response = self._conn.request(method='PUT', endpoint, uid=uid, display_name=display_name,
+                                      key_type=key_type, email=email, access_key=access_key,
+                                      secret_key=secret_key, user_caps=user_caps,
+                                      generate_key=generate_key,
+                                      max_buckets=max_buckets, suspended=suspended)
+        # parse response
 
     def update_user(self, uid, display_name='', email='', access_key='',
                     secret_key='', generate_key='', user_caps='',
