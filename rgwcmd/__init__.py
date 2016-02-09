fcf946dbe286501de16ccc29000b89cbc6813323
Safiyat Reza <md.reza@snapdeal.com>
2016-02-09 19:20:55 +0530

    Ready for build experiments.

--- rgwcmd/__init__.py
+++ rgwcmd/__init__.py
@@ -3,6 +3,16 @@
 __version__ = '0.2'
 __author__ = 'md.reza@snapdeal.com'
 
+from . import commands
+
 def get_version():
     """ Return the current version"""
     return __version__
+
+def main():
+    parser = commands.init_command_parser()
+    args = parser.parse_args()
+    args.func(**commands.namespace_to_dict(args))
+
+if __name__ == '__main__':
+    main()
