e8f13578c19024a13a83e3386f7d80a6f61e9300
Safiyat Reza <md.reza@snapdeal.com>
2016-02-10 19:48:48 +0530

    Merged and updated to be build ready.

--- rgwcmd.py
+++ rgwcmd.py
@@ -1,11 +1,7 @@
 #! /usr/bin/env python
-from rgwcmd import commands
+from rgwcmd import main
 
 
 if __name__ == '__main__':
-   #(navneet) Check the config file from rgwconfig
-   # Use class to check for params
 
-    parser = commands.init_command_parser()
-    args = parser.parse_args()
-    args.func(**commands.namespace_to_dict(args))
+    main()
